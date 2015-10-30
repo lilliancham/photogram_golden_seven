06df49d84f5ddbd942933f3722a028713d3c048a
lilliancham <lilliancham@chicagobooth.edu>
2015-10-29 21:29:09 -0500

    HW

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,40 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params["id"]})
+    render("/photos/show.html.erb")
+  end
+
+  def new_form
+  end
+
+  def create_row
+      p = Photo.new
+      p.caption = params[:the_caption]
+      p.source = params[:the_source]
+      p.save
+
+      redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    i = Photo.find_by({ :id => params[:id]})
+    i.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id => params[:id]})
+  end
+
+  def update_row
+    photo = Photo.find_by({:id => params[:id]})
+    photo.caption = params[:the_caption]
+    photo.source = params[:the_source]
+    photo.save
+    redirect_to("http://localhost:3000/photos/#{photo.id}")
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,24 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Routes to create photos
+  get("/photos/new", { :controller => "photos", :action => "new_form"})
+  get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+  # Routes to READ photos
+  get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  # Routes to edit photos
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  # Routes to delete photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+  # Routes to edit photos
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
 
 end
